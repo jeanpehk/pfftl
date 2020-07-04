b91d31f0d19f02dbdb9041651f4a5373752fadf4
Jere Pehkonen <jere.a.pehkonen@student.jyu.fi>
2020-07-04 22:57:43 +0300

    Sum

--- PART-IV/04-07-2020-sum-tyypit.md
+++ PART-IV/04-07-2020-sum-tyypit.md
@@ -1,3 +1,23 @@
 # Sum-tyypit
 
-Tiedostossa [koodit/Sum.hs](koodit/Sum.hs) on malliesimerkkejä.
+Sum-tyypit voidaan jakaa vastaavasti, kuten [product-tyypit](04-07-2020-product-tyypit.md)
+nullary- ja binäärisummiin sekä rajoitettuihin summiin. Nullarya summaa vastaa määritelmissä
+tyyppi void ja binääriä sum(t1;t2). Void vastaa tyyppiä, jolla ei ole konstruktoria, joten sille
+myöskään ei ole määritelty muotoa, jolla sen voisi muodostaa. Binäärisumma taas sisältää joko
+tyypin t1 tai t2, joten sen muodostukseen on konstruktorit. Binääristä summasta arvo saadaan
+ulos case analyysillä.
+
+Taas kerran ovat myös nullaryt ja binäärit vain erikoistapauksia yleisemmästä rajatusta summasta.
+Rajatussa summassa voidaan pelkän voidin tai binäärisen summan sijaan muodostaa summia, jotka
+ovat mielivaltaisen rajatun kokonaisluvun kokoisia.
+
+Void tyyppi on itselleni aiemminkin aiheuttanut hieman sekaannusta. Kirjassa sitä selitetään
+siten, että void on tyyppi, jolla ei koskaan voi olla arvoa. Siten, jos lausekkeen tyyppi olisi
+void, täytyy olla että lauseke ei tule koskaan saamaan arvoa. Lisäksi, kuten kirjassakin mainitaan,
+sen kanssa aiheuttaa sekaannusta eri kielten eri käsitykset. Esimerkiksi C-kielen void tyyppi vastaisi
+kirjan määritelmien mukaan unit tyyppiä, koska se indikoi, että lausekkeella ei ole mitään
+kiinnostavaa arvoa. Siis toisin kuin void, joka vastaa sitä että lausekkeella ei ole arvoa ollenkaan.
+
+Tiedostossa [koodit/Sum.hs](koodit/Sum.hs) on malliesimerkkejä ja lisäpohdintoja.
+
+Seuraavaksi [pattern matching](05-07-2020-pattern-matching.md).
--- PART-IV/koodit/Sum.hs
+++ PART-IV/koodit/Sum.hs
@@ -1,11 +1,13 @@
 module Sum where
 
+import Data.Void
+
 -- Sum esimerkkejä
 
 -- Haskelissa data-keywordilla voidaan muodostaa
 -- algebraisia tietotyyppejä.
 
--- Tyypilliset esimerkit ovat Either ja Maybe
+-- Tyypilliset esimerkit Either, Maybe ja Bool
 data Either' a b
   = Left' a
   | Right' b
@@ -15,3 +17,39 @@ data Maybe' a
   = Nothing'
   | Just' a
   deriving (Eq, Show)
+
+data Bool'
+  = True'
+  | False'
+  deriving (Eq, Show)
+
+-- Koska Void on tyyppi, jolla ei ole konstruktoria miten
+-- voidaan ikinä muodostaa tyyppiä Void?
+-- Haskelin Data.Void dokumentaatiota kaivamalla löydetään
+-- seuraavat kelpaavat määritelmät, joita en täysin ymmärrä.
+
+data Void' = Void' !Void'
+newtype Void'' = Void'' Void''
+
+-- Ilmeisestikin tässä koska Void on konstruktori ja samalla
+-- argumentti konstruktorille, on todellakin mahdotonta koskaan
+-- muodostaa pelkästään tyyppiä Void.
+
+-- Selkeästi edelleen voidaan muodostaa tyyppi Void -> Void,
+-- mikä voidaan tarkistaa esim. ghci:ssä komennolla ":t Void".
+-- Tämä kuitenkin on loogisesti ihan hyväksyttävää,
+-- sillä ei ole väliä mitä Voidilla voidaan muodostaa, jos sitä
+-- ei itsessään voida muodostaa. Tätä pointtia tukee myös
+-- Data.Voidista löytyvä funktio määritelmä:
+-- absurd :: Void -> a.
+
+-- Toki voitaisiin myös jättää funktio määrittelemättä, jolloin
+-- voidaan funktion tyypiksi asettaa Void. Tämä on kuitenkin
+-- edelleen hyvin kirjan määritelmiä vastaava, sillä funktion
+-- sisältö ei ole tällaisena koskaan suoritettavissa.
+test :: Void
+test = undefined
+
+-- Eikä suoritus luonnollisesti onnistu myöskään absurdin kautta
+test2 :: Void
+test2 = absurd test
