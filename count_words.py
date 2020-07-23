6cc9754acc7d66101ae230a3f379678713f98543
Jere Pehkonen <jere.a.pehkonen@student.jyu.fi>
2020-07-23 14:28:03 +0300

    kattaa myös .txt ja poistettu markdown otsikot aloittavat merkit

--- count_words.py
+++ count_words.py
@@ -4,21 +4,21 @@ from pathlib import Path
 
 # Yksinkertainen ohjelma sanamäärien laskemiseen
 
-# Laskee sanamäärät vain alikansioiden .md tiedostoista, eli jättää esim. koodiesimerkit pois.
+# Laskee sanamäärät vain alikansioiden .md ja .txt tiedostoista, eli jättää esim. koodiesimerkit pois.
 # Tulee huomata myös, että 'sanoiksi' hyväksytään kaikki merkkijonot, joissa välilyönti välissä.
-# Täten esim. '--' on sana, joten tulokset suuntaa antavia.
+# Täten esim. ' -- ' olisi sana, joten tulokset suuntaa antavia.
 
 lens = {}
 
 dirs = [x for x in Path('.').iterdir() if x.is_dir() and x.name != '.git']
 for x in dirs:
-  files = [x for x in Path(x).iterdir() if x.match('*.md')]
+  files = [x for x in Path(x).iterdir() if x.match('*.md') or x.match('*.txt')]
   print('--------------------------------------------------------------------\n')
   print(f'Directory: {x.name}, files: {len(files)}')
   for f in files:
     with open(f) as cf:
-      # rivinvaihdot välilyönneiksi, erotetaan toisistaan ja poistetaan tyhjät
-      data = [x for x in cf.read().replace('\n', ' ').split(' ') if x]
+      # rivinvaihdot välilyönneiksi, erotetaan toisistaan ja poistetaan tyhjät + markdown otsakkeet.
+      data = [x for x in cf.read().replace('\n', ' ').split(' ') if x and x !='#']
       lens[f.name] = len(data)
       print(f'  - {f}, word length: {len(data)}')
   print('\n')
