b90038e5067af95228c8f9eb669ac40def07dad4
Jere Pehkonen <jere.a.pehkonen@student.jyu.fi>
2020-07-24 13:48:11 +0300

    Sing esimerkin siistimistä

--- PART-VIII/koodit/Sing.hs
+++ PART-VIII/koodit/Sing.hs
@@ -1,57 +1,58 @@
 {-# LANGUAGE DataKinds, KindSignatures, GADTs #-}
+
 module Sing where
 
-import Data.Kind -- Jotta voidaan käyttää 'Type' tyyppimääritelmissä.
+import Data.Kind
 
 -- Singletonien pohtimista Haskelissa.
 -- Lisäreferenssinä käytetty Justin Len blogia:
 -- https://blog.jle.im/entry/introduction-to-singletons-1.html
 
--- Ei käytetä valmista singleton kirjastoa vaan koitetaan pohtia
--- asiaa konstruktivistisemmin.
+-- Tehdään blogia ja kirjan esittämää tyyppimuuttujan ongelmaa mallintaen esimerkki
+-- tilan muuttamisesta ja käsittelystä singletonien avulla.
 
--- Tehdään blogia mallintaen esimerkki tilan muuttamisesta
--- ja käsittelystä singletonien avulla.
-
--- Tässä luodaan DataKindsin avulla kind State ja
--- tyyppikonstruktorit 'On ja 'Off
+-- Tässä luodaan DataKindsin avulla kind State ja tyyppikonstruktorit 'On ja 'Off
+-- (On ja Off kelpaa myös, mutta "'" käytetään yleensä selkeyden vuoksi).
 data State = On | Off
   deriving (Show, Eq)
 
-data Machine :: State -> Type where
-  CreateMachine :: { name :: String } -> Machine a
+-- Luodaan tyyppi Machine tyyppimuuttujalla:
+data Machine (s :: State) = CreateMachine { name :: String }
+  deriving (Show, Eq)
 
-create :: State -> String -> Machine a
-create On  = CreateMachine
-create Off = CreateMachine
-
--- Kuten blogissakin, voidaan tässä huomata Haskelin tyyppijärjestelmän
--- rajoitukset. Tyypit ovat olemassa vain staattisen tarkastuksen aikana
--- ja ajonaikana ne hävitetään. Näin ollen meillä ei ole käännösaikana tietoa
--- Machinen tyypistä, koska On ja Off ovat arvoja. Tästä voidaan
--- myös vetää hienoisia johtopäätöksiä aiemmin pikaisesti pohdittujen
--- riippuvasti tyypitettyjen kielten eroista Haskeliin. Olettaisin, että
--- tämä on yhtenä isona rajoituksena, sillä käsitykseni mukaan riippuvasti
--- tyypitetyissä kielissä tyypit nimenomaan riippuvat myös arvoista.
-
--- Tehdään nyt singletonit.
-
-data SLS :: State -> Type where
-  -- Käytetään määritelmissä ' versioita, jotka ovat tyyppikonstruktoreja.
-  SOn  :: SLS 'On
-  SOff :: SLS 'Off
-
--- Nyt meillä on siis singletonit SOn ja SOff, joilla kaikilla
--- on vain yksi instanssi sekä SLS. Lisäksi meillä on mahdollisuus suorittaa
--- pattern match tyyppitasolla arvojen sijaan, mikä mahdollistaa päätösten
--- teon käännösaikana.
-
+-- Voidaan luoda funktioita, jotka muuttavat tilaa kun se on annettu:
 on :: Machine Off -> Machine On
 on (CreateMachine m) = CreateMachine m
 
 off :: Machine On -> Machine Off
 off (CreateMachine m) = CreateMachine m
 
-anyon :: SLS a -> Machine a -> Machine On
-anyon (SOn) m  = m
-anyon (SOff) m = on m
+-- Ongelmia ilmenee, jos tilaa ei tiedetä ennakkoon:
+failAnyOn :: Machine a -> Machine On
+failAnyOn (CreateMachine m) = CreateMachine m
+
+-- Kuten blogissakin, tässä huomataan Haskelin tyyppijärjestelmän
+-- rajoitukset (ja kirjan alustavan esimerkin). Tyypit ovat olemassa vain staattisen
+-- tarkastuksen aikana ja ajonaikana ne hävitetään. Näin ollen meillä ei ole
+-- käännösaikana tietoa Machinen tyyppimuuttujan tyypistä, koska se on funktion
+-- kutsujan määriteltävissä.
+
+-- Tehdään nyt singletonit.
+data SState :: State -> Type where
+  SOn  :: SState 'On
+  SOff :: SState 'Off
+
+-- Nyt voitaisiin ajatella kirjan määritelmiin verraten, että meillä on
+-- 'kind family' SState ja instanssit SOn ja SOff, jotka kattavat vain itsensä.
+
+-- Samalla saadaan mahdollisuus suorittaa pattern match tyyppitasolla arvojen sijaan,
+-- mikä mahdollistaa Machinen tyypin selvittämisen:
+
+successAnyOn :: SState a -> Machine a -> Machine On
+successAnyOn (SOn) m  = m
+successAnyOn (SOff) m = on m
+
+-- Singletonit siis antavat meille mahdollisuuden suorittaa päätöksiä tyyppitasolla
+-- niiden rakenteen mukaan. Ne toimivat ajonaikaisina arvoina, joiden avulla
+-- tyyppipäätökset voidaan tehdä huolimatta siitä, että itse tyypit on ajonaikana
+-- hävitetty!
